67408a88ccf1110a49f5b31ee4e439ebf2dccca4
Kareem <kareemelnaghy@aucegypt.edu>
2023-02-27 18:33:21 +0200

    Prototypes added

--- Assignment 2/main.cpp
+++ Assignment 2/main.cpp
@@ -5,10 +5,24 @@
 //  Created by Kareem Elnaghy on 2/27/23.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <iostream>
+#include "Point.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+const int n = 200;
+const int m = 20;
+
+Point Center(Point *C, int n);
+void closer_Cluster(Point *A, Point *B, const Point &P);
+void GenerateA(Point *A, int n);
+void GenerateB(Point *B, int n);
+void GenerateP(Point *p, int m);
+
+int main()
+{
+    
 }
