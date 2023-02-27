e764a76113ff879a094efe8904260301cc9646c5
Kareem <kareemelnaghy@aucegypt.edu>
2023-02-27 18:18:55 +0200

    Initial Commit

---
+++ Assignment 2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment 2
+//
+//  Created by Kareem Elnaghy on 2/27/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
